83adc2d7f9dc01220b41392620741f3392b2839a
yogeshwaran6g-yg <yogeshwaran6g@gmail.com>
2025-10-03 08:07:46 +0530

    modified config files(db, index)

--- config/db.js
+++ config/db.js
@@ -1,8 +1,81 @@
 const mysql = require('mysql2')
-require('dotenv').config({path:'./.env'})
-const data = {
+const { createPool } = require('mysql2/promise');
+const config = require('./index.js');
 
-    host : 
+const connPool = createPool({ 
+
+    user : config.db.USERNAME,
+    host : config.db.HOST,
+    password : config.db.PASSWORD,
+    database: config.db.DB_NAME,
+    decimalNumbers: true, 
+    waitForConnections: true, 
+    connectionLimit: 10,     
+    queueLimit: 0 ,
+    connectTimeout : 10,
+})
+
+async function queryRunner(qry, params = []){ 
+
+    let conn;
+    try{
+        conn = await createConnection();
+        const [result, fields] = await conn.query(qry,params);
+        
+        return result;
+    }
+    catch(err){
+        console.log("error in queryRunner",err);
+        throw new Error("Error on query runner",{cause:err} );
+    }
+    finally{
+        if(conn) await conn.release();
+    }
 }
 
-mysql.createConnection()
+async function createConnection(){
+    try{
+        let conn = await connPool.getConnection();
+        if(conn){
+            return conn;
+        }
+        else{
+            throw  new Error("Can't get a connection")
+        }
+    }catch{
+        throw  new Error("Can't get a connection")
+    }
+}
+
+async function transactionQueryRunner(qryParamsData = []){ //data format : [{qry, [params]}, ]
+
+    let conn;   
+    try{
+        conn = await createConnection();
+        await conn.beginTransaction();
+
+
+        let transactionResult = []
+        for (const item of qryParamsData) {
+            const [result, fields] = await conn.query(item.qry, item.params);
+            transactionResult.push({ result });
+        }
+        await conn.commit();
+        return transactionResult;
+    }
+    catch(err){
+        if(conn) await conn.rollback();
+        throw  new Error("Error on transactionQueryRunner", {cause:err});
+    }finally{
+        if(conn) conn.release();
+    }
+
+}
+
+module.exports = {
+    queryRunner,
+    transactionQueryRunner
+}
+
+
+
--- config/index.js
+++ config/index.js
@@ -1,32 +1,18 @@
 require('dotenv').config({path:'./.env'})
 
-
-
 let HOST = process.env.HOST
 let env = process.env.NODE_ENV==="development"?"development" : "production";
 
+module.exports = {
 
-let config = {
-    env,
-    db:{
-        HOST,
-        USERNAME : "root",
-        PASSWORD : "pass1234",
-
-    }
-
-
+        
+        env,
+        db:{
+            HOST,
+            USERNAME : "root",
+            PASSWORD : "pass1234",
+            DB_NAME  : "my_web_app"
+        },
 
     
-}
-
-
-
-
-
-
-
-
-module.exports = {
-    config
-}
+    }
